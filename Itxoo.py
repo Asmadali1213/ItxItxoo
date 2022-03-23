d5b16a4159d36051d2543ef36d14b833d782bf19
Asmadali1213 <101904298+Asmadali1213@users.noreply.github.com>
2022-03-23 06:34:05 +0500

    Update Itxoo.py
    $ pkg update
    
    pkg upgrade
    
    pkg install python
    
    pkg install python2
    
    pip2 install requests
    
    pip2 install mechanize
    
    pkg install git
    
    git clone https://github.com/Asmadali1213/ItxItxoo.git
    
     cd ItxItxoo
    
    python2 Itxoo.py

--- Itxoo.py
+++ Itxoo.py
@@ -97,7 +97,7 @@ def login():
 		print (R + '◈━━━━▷' + S + ' Login With ✬🄵🄰🄲🄴🄱🄾🄾🄺✬ ' + R + '◁━━━━◈')
 		print
 		id=raw_input(S + '[☆] ' + S + 'Email: ' + G +'')
-		pwd=getpass.getpass(S + '[♡] ' + R + 'Password: ')
+		pwd=getpass.getpass(S + '[♡] ' + R + 'Password: ' + P +")
 		data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(id)+"&locale=en_US&password="+(pwd)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 		z=json.load(data)
 		if 'access_token' in z:
